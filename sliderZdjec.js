fc2c44373f31744823c83e2eb96715ac203233f8
daniel.roclawski <daniel.roclawski@gmail.com>
2022-06-13 22:12:56 +0200

    zmiany

--- sliderZdjec.js
+++ sliderZdjec.js
@@ -6,11 +6,15 @@ function animacja(){
     var x = document.getElementsByClassName("SliderZdjecia");
     for (i = 0;i<x.length; i++){
         x[i].style.display = "none";
+        x[i].style.transition= "3s";
     }
     liczba++;
     if(liczba>x.length){
         liczba = 1
     }
+    x[liczba-1].style.transition= "3s";
     x[liczba-1].style.display = "block";
+    
     setTimeout(animacja,4000)
+    
 }
