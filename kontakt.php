4045595b0a080cfa0d85e7752df9a745a455a4e2
danielroclawski <93324574+danielroclawski@users.noreply.github.com>
2022-06-29 15:29:37 +0200

    update

--- kontakt.php
+++ kontakt.php
@@ -58,7 +58,7 @@ if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['message
                     <li><a href="galeria.html">Galeria</a></li>
                     <li><a href="index.html#oferta">Oferta</a></li>
                     <li><a href="index.html#sprzet">Sprzęt</a></li>
-                    <li><a href="kontakt.html">Kontakt</a></li>
+                    <li><a href="kontakt.php">Kontakt</a></li>
                </ul>  
         <nav class="nav">
             
@@ -67,7 +67,7 @@ if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['message
         <a href="galeria.html" class="navbar-item">Galeria</a>
         <a href="index.html#oferta" class="navbar-item">Oferta</a>
         <a href="index.html#sprzet" class="navbar-item">Sprzęt</a>
-        <a href="kontakt.html" class="navbar-item">Kontakt</a>
+        <a href="kontakt.php" class="navbar-item">Kontakt</a>
         </nav>
     </div>
     <script src="js/zmianaMenunaBtn.js"></script>
@@ -77,17 +77,18 @@ if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['message
            <h2> Zapraszamy do kontaktu od poniedziałku do soboty w godzinach 8:00-20:00, w pozostałych godzinach preferujemy kontakt mailowy/messanger.<br>
             Marcin Żuchelkowski telefon:<a href="tel:+48691210278">+48 691 210 278</a> email: <a href="mailto:marcin@zuchelkow.ski">marcin@zuchelkow.ski</a> messanger: <a href="http://m.me/USERNAME" target="_blank">Messenger</a></h2>
             <h1>Formularz kontakowy</h1>
-            <form method="post" action="" id="formcontact">
+            <form method="post"  id="formcontact">
                 <label for="name">Imię i Nazwisko:</label><br>
-                <input type="text" name="name" id="name" class="forms"><br>
+                <input type="text" name="name" id="name" class="forms" required><br>
                 <label for="email">E-mail:</label><br>
-                <input type="text" name="email" id="email" class="forms"><br>
+                <input type="text" name="email" id="email" class="forms" required><br>
                 <label for="mess">Treść wiadomści:</label><br>
-                <textarea name="mess" id="mess" class="forms"></textarea><br>
+                <textarea name="message" id="message" class="forms" required></textarea><br>
                 <label for="captcha">Przepisz kod captcha</label>
-<img src="captcha.php" alt="Captcha">
-<input type="text" name="captcha" id="captcha" required>
-                <button class="kontakt-button">WYŚLIJ</button>
+                <img src="captcha.php" alt="Captcha">
+                <input type="text" name="captcha" id="captcha" required>
+                <input type="submit" name="submit" value="Wyślij" class="kontakt-button">
+                <!----<button class="kontakt-button">WYŚLIJ</button> --->
             </form>
            
         </div>
